6116c28784f661454678691f0e81271b635262a0
NaamaK <naama.kapach@gmail.com>
2016-06-16 01:09:40 +0300

    change content-type

--- webService/server.js
+++ webService/server.js
@@ -9,7 +9,7 @@ app.use('/', express.static('./public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.set("Content-Type", "text/html");
+  res.set("Content-Type", "application/json");
   next();
 });
 
