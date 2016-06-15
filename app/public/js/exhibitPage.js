ef94dffc8a38e7633b38a01205a07b08fb61dedc
Shir-G <shirgabai1@gmail.com>
2016-06-15 18:18:40 +0300

    breakthrough with angular

--- app/public/js/exhibitPage.js
+++ app/public/js/exhibitPage.js
@@ -5,15 +5,20 @@ var model;
 app.run(function($http) {
 	$http.get('http://localhost:3000/ws/getExhibits').success(function(data) {
 		model = data;
-		console.log(model);
+		//console.log(model);
 	});
 });
 
-app.controller('exhibitCtrl', function($scope) {
-	$scope.exs = model;
-	$scope.name = model[0].name;
+app.controller('exhibitCtrl', function($scope, $http) {
+    $http.get('http://localhost:3000/ws/getExhibits').then(function(response){
+    
+        $scope.exhibits = response.data;
 
-	$scope.arr= [{i:1}, {i:2},{i:3},{i:4}];
+        //$scope.name = model[1].name;
+
+        //$scope.arr= [{i:1}, {i:2},{i:3},{i:4}];
+    });
+	
 
 
 });
--- webService/exhibit.js
+++ webService/exhibit.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var exhibitSchema = new schema({
-	name: {type:String, index:1, required:true, unique:true},
+	name:  {type:String, index:1, required:true, unique:true},
 	where: String,
-	when: String,
-	size: String,
-	info: String,
+	when:  String,
+	size:  String,
+	info:  String,
 	image: String,
 	video: String,
 	attractions: [Number]
