f7a7ec78d7a7ad56d872f30555e73dbb33408206
NaamaK <naama.kapach@gmail.com>
2016-06-16 00:52:09 +0300

    added exhibit page

--- app/public/js/exhibitPage.js
+++ app/public/js/exhibitPage.js
@@ -5,7 +5,6 @@ var model;
 app.run(function($http) {
 	$http.get('http://localhost:3000/ws/getExhibits').success(function(data) {
 		model = data;
-		//console.log(model);
 	});
 });
 
@@ -13,10 +12,6 @@ app.controller('exhibitCtrl', function($scope, $http) {
     $http.get('http://localhost:3000/ws/getExhibits').then(function(response){
     
         $scope.exhibits = response.data;
-
-        //$scope.name = model[1].name;
-
-        //$scope.arr= [{i:1}, {i:2},{i:3},{i:4}];
     });
 
 });
